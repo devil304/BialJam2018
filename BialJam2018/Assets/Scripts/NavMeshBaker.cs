d17d69ee6dcd10cae1e347db3101c069f66085e0
marcelus1997 <marcinzadykowicz@gmail.com>
2018-06-08 21:56:27 +0200

    warriors2

---
+++ BialJam2018/Assets/Scripts/NavMeshBaker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NavMeshBaker : MonoBehaviour {
+
+    // Use this for initialization
+    public NavMeshSurface[] surfaces;
+    public Transform[] objectsToRotate;
+    public bool toBeBaked = false;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < surfaces.Length; i++)
+        {
+            surfaces[i].BuildNavMesh();
+        }
+    }
+    void Update()
+    {
+        if (toBeBaked)
+        {
+            for (int i = 0; i < surfaces.Length; i++)
+            {
+                surfaces[i].BuildNavMesh();
+            }
+        }
+    }
+}
--- BialJam2018/Assets/Scripts/warrior.cs
+++ BialJam2018/Assets/Scripts/warrior.cs
@@ -5,13 +5,33 @@ using UnityEngine.AI;
 public class warrior : MonoBehaviour {
     public float hp;
     protected NavMeshAgent nma;
+    public Transform[] hymm;
 	// Use this for initialization
 	void Start () {
+        hymm = new Transform[2];
         nma = this.GetComponent<NavMeshAgent>();
-	}
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("Player");
+        for(int i = 0; i < 2; i++)
+        {
+            hymm[i] = temp[i].transform;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        nma.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        
 	}
+    IEnumerator findAndKill()
+    {
+        if (Vector3.Distance(this.transform.position, hymm[1].position) < Vector3.Distance(this.transform.position, hymm[0].position))
+        {
+            nma.destination = hymm[1].position;
+        }
+        else
+        {
+            nma.destination = hymm[0].position;
+        }
+        yield return new WaitForSeconds(5);
+    }
 }
