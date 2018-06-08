6d7a08beab3cc9b484e31b00f85673384b2dea34
marcelus1997 <marcinzadykowicz@gmail.com>
2018-06-08 22:32:10 +0200

    warr&statute

--- BialJam2018/Assets/Scripts/warrior.cs
+++ BialJam2018/Assets/Scripts/warrior.cs
@@ -2,39 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class warrior : MonoBehaviour {
     public float hp;
     protected NavMeshAgent nma;
     public Transform[] hymm;
+    public Transform target;
 	// Use this for initialization
 	void Start () {
-        hymm = new Transform[2];
+        hymm = new Transform[3];
         nma = this.GetComponent<NavMeshAgent>();
         GameObject[] temp = GameObject.FindGameObjectsWithTag("Player");
         for(int i = 0; i < 2; i++)
         {
             hymm[i] = temp[i].transform;
         }
+        hymm[2] = GameObject.FindGameObjectWithTag("Statute").transform;
         StartCoroutine(findAndKill());
     }
 	
 	// Update is called once per frame
 	void Update () {
-
+        nma.destination = target.position;
         
 	}
     IEnumerator findAndKill()
     {
-        Debug.Log("iksde");
         if (Vector3.Distance(this.transform.position, hymm[1].position) < Vector3.Distance(this.transform.position, hymm[0].position))
         {
-            nma.destination = hymm[1].position;
+            if(2*Vector3.Distance(this.transform.position, hymm[1].position)< Vector3.Distance(this.transform.position, hymm[2].position))
+            {
+                target = hymm[1];
+            }
+            else
+            {
+                target = hymm[2];
+            }
+            
         }
         else
         {
-            nma.destination = hymm[0].position;
+            if (2*Vector3.Distance(this.transform.position, hymm[0].position) < Vector3.Distance(this.transform.position, hymm[2].position))
+            {
+                target = hymm[0];
+            }
+            else
+            {
+                target = hymm[2];
+            }
         }
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(0.5f);
         StartCoroutine(findAndKill());
     }
 }
