6514619980989cfa076626e29c5ebe9103b18dc6
paweł <pawelrzosinski@gmail.com>
2018-06-08 23:06:23 +0200

    ryj

--- BialJam2018/Assets/Scripts/boombox.cs
+++ BialJam2018/Assets/Scripts/boombox.cs
@@ -12,13 +12,14 @@ public class boombox : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        hymm = new Transform[2];
+        hymm = new Transform[3];
         nma = this.GetComponent<NavMeshAgent>();
         GameObject[] temp = GameObject.FindGameObjectsWithTag("Player");
         for (int i = 0; i < 2; i++)
         {
             hymm[i] = temp[i].transform;
         }
+        hymm[2] = GameObject.FindGameObjectWithTag("Statute").transform;
         StartCoroutine(findAndKill());
     }
 
@@ -34,13 +35,28 @@ public class boombox : MonoBehaviour
         rh = new RaycastHit[2];
         Physics.Raycast(this.transform.position, hymm[1].position - this.transform.position, out rh[1]);
         Physics.Raycast(this.transform.position, hymm[0].position - this.transform.position, out rh[0]);
-        if (rh[1].transform.gameObject.tag == hymm[1].gameObject.tag&&Vector3.Distance(this.transform.position,hymm[1].position)> Vector3.Distance(this.transform.position, hymm[0].position))
+        if((2*Vector3.Distance(this.transform.position,hymm[2].position)<Vector3.Distance(this.transform.position,hymm[0].position)&& 2 * Vector3.Distance(this.transform.position, hymm[2].position) < Vector3.Distance(this.transform.position, hymm[1].position))||(rh[0].transform.tag!="Player"&& rh[1].transform.tag != "Player"))
         {
-            target.position = hymm[1].position;
+            target.position = hymm[2].position;
+        }
+        else if(rh[1].transform.gameObject.tag == hymm[1].gameObject.tag && rh[0].transform.gameObject.tag == hymm[0].gameObject.tag)
+        {
+            if (Vector3.Distance(this.transform.position, hymm[1].position) > Vector3.Distance(this.transform.position, hymm[0].position))
+            {
+                target.position = hymm[0].position;
+            }
+            else
+            {
+                target.position = hymm[1].position;
+            }
         }
         else if(rh[0].transform.gameObject.tag == hymm[0].gameObject.tag)
         {
             target.position = hymm[0].position;
+        }
+        else
+        {
+            target.position = hymm[1].position;
         }
         yield return new WaitForSeconds(2.5f);
         StartCoroutine(findAndKill());
