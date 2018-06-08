89781ad594e84b049f913753bda2231ca5187ffb
marcelus1997 <marcinzadykowicz@gmail.com>
2018-06-08 21:03:31 +0200

    warrior

---
+++ BialJam2018/Assets/Scripts/warrior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class warrior : MonoBehaviour {
+    public float hp;
+    protected NavMeshAgent nma;
+	// Use this for initialization
+	void Start () {
+        nma = this.GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nma.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+	}
+}
