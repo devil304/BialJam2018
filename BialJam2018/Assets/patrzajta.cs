9e929b9e20186f7237c7677a28ff4dcc6a4361bd
Devil304 <szymonrzosinski@gmail.com>
2018-06-09 13:43:05 +0200

    die die die

---
+++ BialJam2018/Assets/patrzajta.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class patrzajta : MonoBehaviour {
+    public Animator toto;
+	// Use this for initialization
+	void Start () {
+        toto = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            toto.SetInteger("animka", 2);
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            toto.SetInteger("animka", 1);
+        }
+        else
+        {
+            toto.SetInteger("animka", 0);
+        }
+	}
+}
