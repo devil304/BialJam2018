dfc1a0d6d997a7e9f20ef9988dff434c0d8c97d5
paweł <pawelrzosinski@gmail.com>
2018-06-09 08:08:39 +0200

    aa

---
+++ BialJam2018/Assets/Scripts/grow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grow : MonoBehaviour {
+    private float timer = 0;
+    public float rate;
+    public float das1,das2;
+    public float damage;
+	// Use this for initialization
+	public void strat (boom.lol da) {
+        das1 = da.tor1;
+        das2 = da.pren1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.GetComponent<Transform>().localScale = new Vector3(1, 1, 0) * timer * rate;
+        this.transform.Translate(Vector3.forward*das2);
+        if (timer > das1*2)
+        {
+            Destroy(this.gameObject);
+        }
+        timer += Time.deltaTime;
+	}
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag!="pass") {
+            other.gameObject.SendMessage("GetRekt", damage);
+        }
+    }
+}
