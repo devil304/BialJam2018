14ad9127f0db66d376df6e32348e301e1320936a
paweł <pawelrzosinski@gmail.com>
2018-06-10 02:30:15 +0200

    Revert "Revert "asdf""
    This reverts commit 0607134fe7cde3003ab90f5e63a6ebc3e64498b1.

---
+++ BialJam2018/Assets/Scripts/port.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class port : MonoBehaviour {
+    public float hp;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void Damage(float dmg)
+    {
+        hp -= dmg;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
